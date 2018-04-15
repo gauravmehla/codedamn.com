70fe3d3cc01b77d947872696968ccc1e4b17b8b1
Mehul Mohan <mehulmpt@gmail.com>
2018-04-15 17:26:12 +0530

    Added warning message for people not using debug export

--- startup/config.ts
+++ startup/config.ts
@@ -5,6 +5,13 @@ import * as xdebug from 'debug'
 export default () => {
     let isError = false
     const debug = xdebug('cd:startup')
+
+    // Let's warn the developer if debug module's export is not set
+    if(!Config.has('debug')) {
+        console.log(`WARNING: Debug calls are not shown. To show debug calls, use:
+            Mac/Linux: export DEBUG='cd:*'
+            Windows: set DEBUG='cd:*'`)
+    }
     
     // Check if cookieSecret is defined
     if (!Config.has('cookieSecret') || !Config.get('cookieSecret')) {
@@ -32,7 +39,7 @@ export default () => {
 
     // Check if any value is missing then log it on console and terminate the process
     if (isError) {
-        console.log('Please define the environment variables')
-        process.exit(1);
+        debug('Please define the environment variables')
+        process.exit(1)
     }
 }
